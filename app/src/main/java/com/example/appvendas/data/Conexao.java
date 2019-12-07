8b525e3559bb0872e8a6f9b9e9c51cf7d6e920f5
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-12-06 22:17:52 -0300

    Update sql

--- app/src/main/java/com/example/appvendas/data/Conexao.java
+++ app/src/main/java/com/example/appvendas/data/Conexao.java
@@ -17,6 +17,10 @@ public class Conexao extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE cliente(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, numero TEXT NOT NULL)");
+
+        db.execSQL("CREATE TABLE venda(id INTEGER PRIMARY KEY AUTOINCREMENT, quantidade INTEGER NOT NUll, descricao TEXT NOT NULL, data TEXT NOT NULL, preco FLOAT NOT NULL, idCliente INTEGER, FOREIGN KEY (idCliente) REFERENCES cliente(id))");
+
+        //db.execSQL("SELECT * FROM venda WHERE idCliente = id");
     }
 
     @Override
