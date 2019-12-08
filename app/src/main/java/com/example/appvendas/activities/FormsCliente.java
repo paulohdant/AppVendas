6a049657ce70114524940cc3d3001a2a5649b5eb
Kmiokande <brunnodesouzabezerra@gmail.com>
2019-12-07 22:58:41 -0300

    Forms cliente finalizado e funcionando

--- app/src/main/java/com/example/appvendas/activities/FormsCliente.java
+++ app/src/main/java/com/example/appvendas/activities/FormsCliente.java
@@ -2,14 +2,10 @@ package com.example.appvendas.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-//import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.example.appvendas.models.Cliente;
@@ -17,55 +13,52 @@ import com.example.appvendas.data.ClienteDAO;
 import com.example.appvendas.R;
 
 public class FormsCliente extends AppCompatActivity {
-
-    private Integer id;
-    private EditText nome;
-    private EditText numero;
+    private EditText etNomeCliente;
+    private EditText etNumeroCliente;
     private Button btSalvarCliente;
+    private Cliente cliente = new Cliente();
     private ClienteDAO dao;
-    private Cliente cliente = null;
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_principal, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forms_cliente);
-        nome = findViewById(R.id.etNomeCliente);
-        numero = findViewById(R.id.etNumeroCliente);
-        btSalvarCliente = findViewById(R.id.btSalvarCliente);
+        carregarComponentes();
+
         dao = new ClienteDAO(this);
 
-        Intent i = getIntent();
-        if(i.hasExtra("cliente")){
-            cliente = (Cliente) i.getSerializableExtra("cliente");
-            nome.setText(cliente.getNome());
-            numero.setText(cliente.getNumero());
-
-        }
+        btSalvarCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(etNomeCliente.getText().toString().isEmpty() || etNumeroCliente.getText().toString().isEmpty()) {
+                    Toast.makeText(FormsCliente.this, "Preencha todos os dados antes de salvar!", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    salvar();
+                }
+            }
+        });
     }
 
-//    public void setup(View view) {
-//        if(cliente == null) {
-//            cliente = new Cliente();
-//            cliente.setNome(nome.getText().toString());
-//            cliente.setNumero(numero.getText().toString());
-//            long id = dao.inserir(cliente);
-//            Toast.makeText(this, "Pessoa inserida: " + id, Toast.LENGTH_SHORT).show();
-//            System.out.println(cliente.getNome() + cliente.getNumero());
-//            finish();
-//        }else{
-//            cliente.setNome(nome.getText().toString());
-//            cliente.setNumero(numero.getText().toString());
-//            dao.atualizarClienteDAO(cliente);
-//            Toast.makeText(this, "Pessoa atualizada: ", Toast.LENGTH_SHORT).show();
-//            finish();
-//        }
-//    }
+    private void carregarComponentes() {
+        etNomeCliente = findViewById(R.id.etNomeCliente);
+        etNumeroCliente = findViewById(R.id.etNumeroCliente);
+        btSalvarCliente = findViewById(R.id.btSalvarCliente);
+    }
+
+    private void salvar() {
+        try {
+            String nome = etNomeCliente.getText().toString();
+            String numero = etNumeroCliente.getText().toString();
+
+            cliente.setNome(nome);
+            cliente.setNumero(numero);
+
+            dao.inserir(cliente);
+            finish();
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Erro ao salvar cliente!", Toast.LENGTH_LONG).show();
+        }
+    }
 }
